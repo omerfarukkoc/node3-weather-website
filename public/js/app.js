0e96138e7c2914dba3d26aa97774833855f95dfe
omerfarukkoc <omerfarukkoc@hotmail.com>
2020-06-21 16:31:23 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,3 @@
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -30,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error){
             messageOne.textContent = data.error
